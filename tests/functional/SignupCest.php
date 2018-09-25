ab67c6a6067d5bc9f9353107316eced34c0d732e
Alexey Schevchenko <ivanovosity@gmail.com>
2018-09-25 21:57:57 +0300

    add tests

---
+++ tests/functional/SignupCest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Class SignupCest
+ */
+class SignupCest
+{
+    /**
+     * @var string
+     */
+    protected $formId = '#form-signup';
+
+    /**
+     * @inheritdoc
+     * @param FunctionalTester $I
+     */
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnRoute('site/signup');
+    }
+
+    /**
+     * @inheritdoc
+     * @param FunctionalTester $I
+     */
+    public function signupWithEmptyFields(FunctionalTester $I)
+    {
+        $I->see('Sign Up', 'h1');
+        $I->see('Please fill in the following fields to sign up:');
+        $I->submitForm($this->formId, []);
+        $I->see('Username cannot be blank.');
+        $I->see('Password cannot be blank.');
+
+    }
+
+    /**
+     * @inheritdoc
+     * @param FunctionalTester $I
+     */
+    public function signupWithWrong(FunctionalTester $I)
+    {
+        $I->submitForm(
+            $this->formId, [
+                'SignupForm[username]' => 'erau',
+                'SignupForm[password]' => 'wrong',
+            ]
+        );
+        $I->dontSee('Username cannot be blank.', '.help-block');
+        $I->dontSee('Password cannot be blank.', '.help-block');
+    }
+
+    /**
+     * @inheritdoc
+     * @param FunctionalTester $I
+     */
+    public function signupSuccessfully(FunctionalTester $I)
+    {
+        $I->submitForm($this->formId, [
+            'SignupForm[username]' => 'testers',
+            'SignupForm[password]' => '123456',
+        ]);
+
+        $I->see('Thank you for registering.');
+    }
+}
